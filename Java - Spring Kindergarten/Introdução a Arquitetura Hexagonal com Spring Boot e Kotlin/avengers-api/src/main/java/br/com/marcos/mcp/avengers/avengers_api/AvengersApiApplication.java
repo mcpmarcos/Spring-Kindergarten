5e33d958103ecb036a5b9c02e91b8257b1be3a91
Marcos Pacheco <mcp_marcos@hotmail.com>
2026-05-13 22:19:50 -0300

    Iniciando um ajornada no aprendizado sobre arquitetura hexagonal

---
+++ Java - Spring Kindergarten/Introdução a Arquitetura Hexagonal com Spring Boot e Kotlin/avengers-api/src/main/java/br/com/marcos/mcp/avengers/avengers_api/AvengersApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcos.mcp.avengers.avengers_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvengersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvengersApiApplication.class, args);
+	}
+
+}
---
+++ Java - Spring Kindergarten/Introdução a Arquitetura Hexagonal com Spring Boot e Kotlin/avengers-api/src/test/java/br/com/marcos/mcp/avengers/avengers_api/AvengersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.mcp.avengers.avengers_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvengersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
